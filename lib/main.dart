119fad61c74756c06343d213150a5dfb26ea8d26
Ecky819 <marcoeggert73@gmail.com>
2025-07-24 10:09:03 +0200

    main.dart erstellt

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,49 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
+  class _MyAppState extends State<MyApp {
+    int _selectedIndex = 0;
+
+    late final List<Widget> _screens;
+
+    @override
+    void initState() {
+      super.initState();
+      _screens = [
+        ImageScreen(onNavigate: _onItemTapped),
+        AboutScreen(onNavigate: _onItemTapped),
+      ];
+    }
+    void _onItemTapped(int index) {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    return MaterialApp(
+      title: 'App Gallery',
+      theme: ThemeData(
+        primarySwatch: Colors.red, // Primärfarbe der App
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        fontFamily: 'Inter', // Verwendung der Schriftart Inter
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.red[700], // Hintergrundfarbe der AppBar
+          foregroundColor: Colors.white, // Textfarbe der AppBar
+          elevation: 4.0, // Schatten der AppBar
+        ),
+        cardTheme: CardThemeData(
+          elevation: 4.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(
+              12.0,
+            ), // Abgerundete Ecken für Karten
+          ),
+        ),
+      ),
+      home:
+          _screens[_selectedIndex], // Zeigt den aktuell ausgewählten Bildschirm an
+      debugShowCheckedModeBanner: false, // Debug-Banner ausblenden
     );
   }
 }
